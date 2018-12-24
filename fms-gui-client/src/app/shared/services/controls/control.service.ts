3830fe0976795db85514223e11781feff77e6d64
Thomas <tkhlebovitch@hotmail.com>
2018-12-24 17:12:33 +0100

    FMS-C5 Create cards, which contain the information in the status panel, create schema for cards

--- fms-gui-client/src/app/shared/services/controls/control.service.ts
+++ fms-gui-client/src/app/shared/services/controls/control.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {NavItem} from '../../model/nav-item.model';
 import {StatusControl} from '../../model/status-control.model';
 
 /**
